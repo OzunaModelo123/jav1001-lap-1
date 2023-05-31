5cf9005ea55c2562d83a37ff3009f0677ec87dcf
Daniel Ozoani <57331891+OzunaModelo123@users.noreply.github.com>
2023-05-31 12:36:32 -0400

    Collect a numeric value from the user

---
+++ Convert.kt
@@ -0,0 +1,7 @@
+fun main() {
+    println("Welcome to the Measurement Conversion Program!")
+
+    // Collect a numeric value from the user
+    print("Enter the numeric value: ")
+    val value = readLine()?.toDoubleOrNull()
+}
